8eb5baab2d6dd8e8d588f92ad61f5f3cd8ea956f
BatuhanSatilmis <afrousse1998@gmail.com>
2022-10-17 03:28:09 +0300

    commit

---
+++ src/components/Find/FInd.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './FInd.css'
+const FInd = () => {
+    return (
+        <div className='find'>
+            <h1>FInd me on Github</h1>
+            <p><a href="https://github.com/BatuhanSatilmis">@Batuhan Satilmis</a></p>
+        </div>
+    );
+};
+
+export default FInd;
